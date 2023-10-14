b917dec09867de8a8d79d8c724660b5cdb9b99ac
Erik Rusanov <erik.rusanov.03@gmail.com>
2023-10-14 09:06:32 +0300

    Poisson distribution to exponential

--- 02_231013/poiss.py
+++ 02_231013/poiss.py
@@ -8,7 +8,7 @@ from ipywidgets import IntSlider, interactive, FloatSlider, Output
 class PoissonProcessSimulator:
     def __init__(self):
         self.interval = IntSlider(value=100, min=20, max=150, step=1, description="Time")
-        self.param = FloatSlider(value=10, min=0.1, max=20, step=0.1, description="λ")
+        self.param = FloatSlider(value=1, min=0.1, max=2, step=0.1, description="λ")
 
         self.interactive_plot = interactive(
             self.update_plot,
@@ -23,7 +23,7 @@ class PoissonProcessSimulator:
         while True:
             number += 1
             start_time = 0 if not segments else segments[-1][0][1]
-            end_time = start_time + np.random.poisson(param)
+            end_time = start_time + np.random.exponential(param)
             if end_time > interval:
                 break
 
